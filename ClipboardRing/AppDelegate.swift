c90b73e19876321e31b807fc5ea075b51b9780c5
Elton Lika <eltonlika1@gmail.com>
2019-04-05 10:55:59 +0200

    Initial Commit

---
+++ ClipboardRing/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ClipboardRing
+//
+//  Created by Elton Lika on 5/4/19.
+//  Copyright © 2019 Elton Lika. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
